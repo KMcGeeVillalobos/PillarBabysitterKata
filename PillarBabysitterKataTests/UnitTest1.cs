d598e869b7cf814667e8f752c628996843358830
KMcGeeVillalobos <kristin.villalobos@gmail.com>
2018-12-31 12:44:37 -0500

    file created

---
+++ PillarBabysitterKataTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PillarBabysitterKataTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
